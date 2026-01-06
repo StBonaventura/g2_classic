9c25dbfbd54f1e80a895eb59b44582188235cfba
StBonaventura <thesearingwind@gmail.com>
2026-01-06 21:50:33 +0300

    removed free armor from gorn because hidden armor in cell that was probably searched by paladins is implausible

--- PrjGothic/Story/Dialoge/DIA_pc_fighter_ow.d
+++ PrjGothic/Story/Dialoge/DIA_pc_fighter_ow.d
@@ -49,18 +49,7 @@ func void dia_gornow_hello_info()
 	AI_Output(self,other,"DIA_GornOW_Hello_12_02");	//Но я совсем не ожидал, что это будешь ты, впрочем. Я чертовски рад видеть тебя.
 	AI_Output(other,self,"DIA_GornOW_Hello_15_03");	//(ухмыляется) Гаронд говорит, что ты ешь слишком много и он больше не может себе позволить кормить тебя.
 	AI_Output(self,other,"DIA_GornOW_Hello_12_04");	//Раз уж ты упомянул об этом, я бы не отказался от пары кружек пива. Но нужно выбираться отсюда. Эта камера мне уже порядком надоела.
-	if(other.guild == GIL_SLD)
-	{
-		AI_Output(self,other,"DIA_GornOW_Add_12_00");	//Подожди, у меня есть кое-что, что могло бы пригодиться тебе.
-		AI_Output(self,other,"DIA_GornOW_Add_12_01");	//Я нашел эти доспехи здесь, в камере. Наверное, их спрятал какой-то заключенный.
-		b_giveinvitems(self,other,itar_sld_m,1);
-		AI_Output(self,other,"DIA_GornOW_Add_12_02");	//Для меня они слишком малы, но тебе могут подойти ...
-		AI_Output(other,self,"DIA_GornOW_Add_15_03");	//Спасибо! Увидимся у Милтена ...
-	}
-	else
-	{
-		AI_Output(other,self,"DIA_GornOW_Hello_15_05");	//Ладно, увидимся у Милтена.
-	};
+	AI_Output(other,self,"DIA_GornOW_Hello_15_05");	//Ладно, увидимся у Милтена.
 	AI_Output(self,other,"DIA_GornOW_Hello_12_06");	//Конечно.
 	AI_StopProcessInfos(self);
 	Npc_ExchangeRoutine(self,"FREE");
